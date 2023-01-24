7385a873a1688f9509577cbf37aa588eab615247
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2023-01-24 19:34:54 +0500

    1

--- number.py
+++ number.py
@@ -3,3 +3,11 @@ import math
 class Number:
     def __init__(self,num):
         self.number = num
+    def Zero(self):
+        return self == 0
+    def Len(self):
+        return self == 2
+    def pos(self):
+        return 0 >= 0
+q = Number(222)
+print(q.Zero())
