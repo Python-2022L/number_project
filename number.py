1204e325840c9d8e8a79491021a6fd0c3da1f777
user1 <backenduz@gmail.com>
2023-01-24 19:33:30 +0500

    solved

--- number.py
+++ number.py
@@ -3,3 +3,24 @@ import math
 class Number:
     def __init__(self,num):
         self.number = num
+
+    def data_type(self):
+        return type(self.number)
+    
+    def len(self):
+        return len(self.number)
+    
+    def is_positive(self):
+        return self.number > 0
+    
+    def is_negative(self):
+        return self.number < 0
+    
+    def is_zero(self):
+        return self.number == 0
+    
+    def is_even(self):
+        return  self.number % 2 == 0
+    
+    def is_odd(self):
+        return self.number % 2 != 0
