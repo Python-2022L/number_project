14ece0f8bfffaaa7068b6243074380c0f1251799
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2023-01-24 19:35:56 +0500

    positive

--- number.py
+++ number.py
@@ -3,3 +3,15 @@ import math
 class Number:
     def __init__(self,num):
         self.number = num
+
+    def is_positive(self):
+        return self.number>0
+
+
+
+    
+
+x = Number(10)
+
+print(x.is_positive())
+
