4b208e60505d95effeaa6ecc5e6f908098bf8181
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-01-24 19:35:01 +0500

    number.py

--- number.py
+++ number.py
@@ -3,3 +3,35 @@ import math
 class Number:
     def __init__(self,num):
         self.number = num
+    
+    def positive(self):
+        """
+        check if the number is positive
+        """
+        return self.number>=0
+    def negative(self):
+        """
+        check ifi the number is negative
+        """
+        return self.number<0
+    def len(self):
+        """
+        len
+        """
+        return len(self.number)
+    def even(self):
+        """
+        check if the number is even
+        """
+        return self.number%2==0
+    def odd(self):
+        """
+        check if the number is odd"""
+        return self.number%2==1
+    def zero(self):
+        """
+        check if the number is zero
+        """
+        return self.number == 0
+x = Number(20)
+print(x.negative())
